91cc2623dc5bbaaa5942962e7947374c1d2977cd
Nig <stu226843@mail.uni-kiel.de>
2024-07-24 21:47:43 +0200

    dockerfile update

--- Webstuff/app.py
+++ Webstuff/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
-import database.database as db
+from database import database as db
 from utils import Field
 from config import Config
 from forms import LoginForm
---
+++ database/__init__.py
@@ -0,0 +1,2 @@
+from database import *
+from .database import Database
