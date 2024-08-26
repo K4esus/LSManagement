cfedb1354a3a8e2577dd0e94faea08dd74c46285
Nig <stu226843@mail.uni-kiel.de>
2024-08-26 16:12:28 +0200

    DB restart überarbeitet

--- database/DB_restarter.py
+++ database/DB_restarter.py
@@ -7,15 +7,17 @@ Drops the feldaten table and recreates it with the mockdata
 
 CONNECTION = sqlite3.connect("main.db")
 # drops all tables
-CONNECTION.execute("DROP TABLE IF EXISTS felddaten")
+#CONNECTION.execute("DROP TABLE IF EXISTS felddaten")
 CONNECTION.execute("DROP TABLE IF EXISTS user")
 
 # adds user and felddaten table
+"""
 CONNECTION.execute(
             "CREATE TABLE IF NOT EXISTS felddaten "
             "(fieldnumber INTEGER PRIMARY KEY, crop TEXT,precrop TEXT,cycle TEXT,lime TEXT,"
             "fertilizer TEXT,plow TEXT,roll TEXT,status TEXT,fieldsize REAL)"
         )
+
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS user "
                    "(username TEXT PRIMARY KEY, password_hash TEXT, admin BOOL)")
 
@@ -25,5 +27,13 @@ with open("../testdata/Mockdata2.csv") as f:
         if index > 1:
             print(row)
             CONNECTION.execute("INSERT INTO felddaten VALUES (?,?,?,?,?,?,?,?,?,?)", row)
+"""
 
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS user "
+                   "(username TEXT PRIMARY KEY, password_hash TEXT, admin BOOL)")
+username = "Nig"
+password = "LSMANAGEMENT"
+admin = True
+password_hash = generate_password_hash(password)
+CONNECTION.execute("INSERT INTO user VALUES (?, ?, ?)", (username, password_hash, admin))
 CONNECTION.commit()
